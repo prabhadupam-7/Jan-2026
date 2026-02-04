abf18dd703fef9eedf06b04a4e5316f5f49d7cb0
prabhadupam-7 <prabhadupam.7@gmail.com>
2026-02-04 00:42:24 -0500

    day 3

--- javascript/Day_03/addition.js
+++ javascript/Day_03/addition.js
@@ -1,5 +1,5 @@
-let a = 10;
+﻿let a = 10;
 let b = 20;
 console.log(`Addition of ${a} and ${b} is ${a + b}`); // works in both browser & nodejs
 
-window.alert(`Addition of ${a} and ${b} is ${a + b}`); // works in browser & not in nodejs
+//window.alert(`Addition of ${a} and ${b} is ${a + b}`); // works in browser & not in nodejs
