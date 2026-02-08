22555495166d02cbb82386ba9a58bc4e26b53ccf
prabhadupam-7 <prabhadupam.7@gmail.com>
2026-02-08 02:31:20 -0500

    till day 9 assignemt

--- javascript/Day_02/addition.js
+++
@@ -1,8 +0,0 @@
-
-    let a=10;
-    let b=20;
-    console.log(`Adition of ${a} and ${b} is ${a+b}`); //works both in browser and node js
-    document.write(`Adition of ${a} and ${b} is ${a+b}`);//works only in browser and not in node js
-    window.alert(`Adition of ${a} and ${b} is ${a+b}`); //works only in browser and not in node js
-
-  
